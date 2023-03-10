f2552e97890a3b5dd14189a1192a7a34e505ea09
Terrence <terrthetingler240@proton.me>
2023-03-10 00:18:37 -0600

    Add files via upload

---
+++ DiscordLinkScraper.py
@@ -0,0 +1,25 @@
+import json
+import re
+import pyperclip
+
+# Load the JSON data from the file
+with open('Chat Exports\link-queue.json') as f:
+    data = json.load(f)
+
+links = []
+for message in data['messages']:
+    # Extract links from message content
+    links_in_message = re.findall(r'(?:https?://|www\.)\S+', message['content'])
+    # Append links to the list of links
+    links.extend(links_in_message)
+
+if links:
+    # Convert the list of links to a string separated by newline character
+    links_string = '\n'.join(links)
+    # Copy the links string to the clipboard
+    pyperclip.copy(links_string)
+    # Print the number of links copied to the clipboard
+    print(f"Copied {len(links)} links to clipboard.")
+else:
+    # If no links were found, print "No links found in the message(s)."
+    print("No links were found in the message(s).")
